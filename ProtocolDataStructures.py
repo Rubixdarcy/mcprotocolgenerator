7142502fce8be8166a9ef59d815744c05b016719
Jeremiah Ryan <jemryan95@gmail.com>
2018-02-06 13:06:08 +1100

    Add submodule for protocol.json

--- ProtocolDataStructures.py
+++ ProtocolDataStructures.py
@@ -1,8 +1,12 @@
 from construct import *
 
-slot = Struct(
+Slot = Struct(
     "blockId" / Int16,
     "itemCount" / If(this.blockid > -1, Int8),
     "itemDamage" / If(this.blockid > -1, Int16),
-    "nbtData" / If(this.blockid > -1, OptioanlNbt)
-    )
+    "nbtData" / If(this.blockid > -1, OptioanlNbt))
+
+Position = BitStruct(
+    "x" / BitsInteger(26, signed=True),
+    "y" / BitsInteger(12, signed=True),
+    "z" / BitsInteger(26, signed=True))
